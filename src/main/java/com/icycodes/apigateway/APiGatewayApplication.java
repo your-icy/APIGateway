0260d5770ec9a5beaffa0ffab703c51cadf9db80
your-icy <dev.harshpal@gmail.com>
2024-06-29 03:50:54 +0530

    "06/29/2024 - API Gateway Integration"

---
+++ src/main/java/com/icycodes/apigateway/APiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.icycodes.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icycodes/apigateway/APiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icycodes.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
